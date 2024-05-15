713c68d9796e46647dd404ac6c6ff3b976a90a76
rshar99 <168565364+rshar99@users.noreply.github.com>
2024-05-15 14:24:40 -0400

    Create adminhomepage.php

---
+++ WebApp/adminhomepage.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once('adminnavbar.php');
+
+?>
+
+<div class="container" style="" >
+<center>
+<img src="img/dashboard.png" class="img img-responsive" />
+<h2 class="text-danger">Welcome to Admin Panel</h2>
+</center>
+</div>
