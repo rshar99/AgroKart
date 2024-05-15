ead55c7ee18203e727d58a02d838bb914a580399
rshar99 <168565364+rshar99@users.noreply.github.com>
2024-05-15 14:28:27 -0400

    Create dealerhomepage.php

---
+++ WebApp/dealerhomepage.php
@@ -0,0 +1,17 @@
+<?php
+
+	include_once('dealernavbar.php');
+
+?>
+
+<div style="height: 90vh" class="text-primary" >
+        <div class="flex-center flex-column" style="">
+		
+			<img src="img/dealerr.jpg" class="img-fluid img-circle" />
+            <h1 class="animated fadeIn mb-4"><center>Welcome </br> To </center>Agro-Kart</h1>
+
+            <h5 class="animated fadeIn mb-3">Thank you for using our application. We're glad you're with us.</h5>
+
+            <p class="animated fadeIn text-muted">Agro-Kart Team</p>
+        </div>
+    </div>
