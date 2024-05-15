909531a4d1c92cf389d24e93a7030e602f163341
rshar99 <168565364+rshar99@users.noreply.github.com>
2024-05-15 16:27:53 -0400

    Create farmerhomepage.php

---
+++ WebApp/farmerhomepage.php
@@ -0,0 +1,17 @@
+<?php
+	
+	include_once('farmernavbar.php');
+?>
+      
+	<div style="height: 90vh">
+        <div class="flex-center flex-column" style="">
+		
+			<img src="img/farmerbanner.jpg" class="img-fluid img-circle" />
+            <h4 class="text-danger"><center>Welcome To Agro-Kart</center></h4>
+
+            
+			<center><h5 class="">Thank you for using our application. We're glad you're with us.</h5></center>
+
+            <p class="animated fadeIn text-muted">Agro-Kart Team</p>
+        </div>
+    </div>
