c5ea31e6134205e460eb48c0595228a15430315b
rshar99 <168565364+rshar99@users.noreply.github.com>
2024-05-15 16:35:05 -0400

    Create videogal.php

---
+++ WebApp/videogal.php
@@ -0,0 +1,41 @@
+
+	 <?php
+	 
+	 include_once('farmernavbar.php');
+	 
+	 ?>
+	    
+
+	  
+	  <br/><br/>
+	  <div class="col-xs-12 a">
+	      
+	      
+	      <iframe width="100%" height="" src="https://www.youtube.com/embed/NHGXCrpjEZI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	      
+	      
+	  </div>  
+	    
+	  <br/><br/>
+	  
+	 <div class="col-xs-12 a">
+	     
+	     
+	    <iframe width="100%" height="" src="https://www.youtube.com/embed/e0yQ-oEdTKE?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	 </div> 
+	 
+	 
+	 <br/><br/>
+	 
+	 <div class="col-xs-12 a" >
+	 <iframe width="100%" height="" src="https://www.youtube.com/embed/9t17cnwS2SM?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	     
+	   </div>
+	 
+	 
+	 
+	    
+	</body>
+	
+	
+	
